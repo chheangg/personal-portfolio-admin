fd6b7c263d1156448345f36cfe59855027bc8dbe
chheangg <balldremz@gmail.com>
2023-02-22 14:46:51 +0700

    Add Grid component to layout

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,8 +1,16 @@
+import { Grid, GridItem } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => 
-  <div>
-    <Outlet />
-  </div>
+  <Grid templateColumns='1fr 4fr' gap='4'>
+    <GridItem>
+      <aside>
+        side bar
+      </aside>
+    </GridItem>
+    <GridItem>
+      <Outlet />
+    </GridItem>
+  </Grid>
 
 export default Layout
